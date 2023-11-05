6dfbdf2fc231fbd35ee9b1c900737a192587b50d
Dmytro Sayko <saykodmytro@icloud.com>
2023-11-05 11:39:54 +0200

    up

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = '';
-axios.defaults.baseURL = 'https://api.pexels.com/v1/';
-axios.defaults.headers.common['Authorization'] = API_KEY;
+const API_KEY = "LqdMyZMOt2jhKLy7gSpMcnkKi2ysGicFxR641v833ReVTacqB15tsu02";
+axios.defaults.baseURL = "https://api.pexels.com/v1/";
+axios.defaults.headers.common["Authorization"] = API_KEY;
 axios.defaults.params = {
-  orientation: 'landscape',
+  orientation: "landscape",
   per_page: 15,
 };
 
-export const getImages = (query, page) => {};
+export const getImages = async (query, page) => {
+  const { data } = await axios.get(`search?query=${query}&page=${page}`);
+  return data;
+};
